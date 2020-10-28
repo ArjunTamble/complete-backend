d906e86bf7725452ae5520765da37f5e57aaf8b5
ArjunTamble <arjuntamble@gmail.com>
2020-10-28 21:29:07 +0530

    added promos and leaders

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const dishRouter = require('./public/routes/dishRouter');
+const promoRouter = require('./public/routes/promoRouter');
+const leaderRouter = require('./public/routes/leaderRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -13,6 +15,8 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 app.use('/dishes', dishRouter);
+app.use('/promos', promoRouter);
+app.use('/leaders', leaderRouter);
 
 app.use(express.static(__dirname + '/public'));
 

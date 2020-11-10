ff7faf96e4b97f91173402ff6b04e74e507cb2ef
ArjunTamble <arjuntamble@gmail.com>
2020-11-10 14:59:00 +0530

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '824318005038580',
+        clientSecret: 'af169ad09055595b46c960768b433eb9'
+    }
 }
